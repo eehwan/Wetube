0901589448177c206703af5136f6a973ea197885
eehwan <eehwan.shin@gmail.com>
2020-08-20 17:57:48 +0900

    add .babelrc

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hi");
+import express from "express";
+const app = express();
+const port_num = 5006;
+
+// handle function
+const handleHome = (req, res) => res.send("Hello from Home!");
+const handleProfile = (req, res) => res.send("You are on my profile");
+const handleListening = () =>
+  console.log(`Listening on port: ${port_num}`);
+
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+// port
+app.listen(port_num, handleListening);
