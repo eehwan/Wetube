ddb51a3d2dc63ccb9b9c62289fe5b0e33cb5c582
eehwan <eehwan.shin@gmail.com>
2020-08-21 22:05:47 +0900

    20-08-21 #2

--- app.js
+++ app.js
@@ -3,25 +3,28 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookie_parser from "cookie-parser";
 import body_parser from "body-parser";
-import { userRouter } from "./Router/user";
 
+import globalRouter from "./Routers/globalRouter";
+import userRouter from "./Routers/userRouter";
+import videoRouter from "./Routers/videoRouter";
+import routes from "./routes";
+
+import { globalVariables } from "./globalVariables"
 const app = express();
 
-// handle function
-const handleHome = (req, res) => res.send("Hello from Home!");
-const handleProfile = (req, res) => res.send("You are on my profile");
-
 //middle ware
+app.use(helmet());
+app.set('view engine', "pug")
 app.use(cookie_parser());
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended: true}));
-app.use(helmet())
-app.use(between);
 app.use(morgan("combined"));
 
+app.use(globalVariables)
+
 // route
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
