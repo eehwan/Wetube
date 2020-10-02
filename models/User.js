140fb7a591d944e17a3fc20e87d0c8627178a347
eehwan <eehwan.shin@gmail.com>
2020-10-02 23:58:26 +0900

    db.user - add loginType

--- models/User.js
+++ models/User.js
@@ -7,6 +7,7 @@ const UserSchema = new mongoose.Schema({
   avatarUrl: String,
   facebookId: Number,
   githubId: Number,
+  loginType: String,
 });
 UserSchema.plugin(passportLocalMongoose, { usernameField: "email" });
 
