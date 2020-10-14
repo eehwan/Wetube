1ed7ff12616b97f222586bf92523d388db95e481
eehwan <eehwan.shin@gmail.com>
2020-10-15 00:11:18 +0900

    edited recording function

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -3,24 +3,48 @@ const recordContainer = document.querySelector("#jsRecordContainer");
 function init() {
   const videoPreview = recordContainer.querySelector("#jsVideoPreview");
   const recordBtn = recordContainer.querySelector("#jsRecordBtn");
-  const startRecording = async () => {
+  let stream;
+  let videoRecorder;
+  const getRecord = async () => {
     try {
-      const stream = await navigator.mediaDevices.getUserMedia({
+      stream = await navigator.mediaDevices.getUserMedia({
         audio: true,
         video: { width: 1280, height: 720 },
       });
       videoPreview.srcObject = stream;
       videoPreview.muted = true;
       videoPreview.play();
+      recordBtn.innerHTML = `⛔ Stop Recording`;
+      startRecording();
     } catch (error) {
       recordBtn.innerHTML = `❌ ${error}`;
-      // recordBtn.innerHTML = "❌ Can't Record";
-      // recordBtn.removeEventListener("click", startRecording);
     }
   };
-
+  const startRecording = () => {
+    recordBtn.removeEventListener("click", getRecord);
+    recordBtn.addEventListener("click", stopRecording);
+    videoRecorder = new MediaRecorder(stream);
+    videoRecorder.start();
+    videoRecorder.addEventListener("dataavailable", handleVideoData);
+  };
+  const stopRecording = () => {
+    videoRecorder.stop();
+    stream.getTracks().forEach((track) => track.stop());
+    stream = null;
+    recordBtn.innerHTML = `🎬 Start Recording`;
+    recordBtn.removeEventListener("click", stopRecording);
+    recordBtn.addEventListener("click", getRecord);
+  };
+  function handleVideoData(event) {
+    const { data: videoFile } = event;
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(videoFile);
+    link.download = "recorded.webm";
+    document.body.appendChild(link);
+    link.click();
+  }
   // main
-  recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getRecord);
 }
 
 if (recordContainer) {
