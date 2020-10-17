582c5a8c3915f1bfd9aeee590988ce4b43e33a9d
eehwan <eehwan.shin@gmail.com>
2020-10-17 20:26:37 +0900

    edited video-comments function

--- assets/js/videoComment.js
+++ assets/js/videoComment.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 const addCommentForm = document.querySelector("#jsAddComment");
+const commentList = document.querySelector("#jsCommentList");
+const commentNumber = document.querySelector("#jsCommentNumber");
 
 const sendComment = async (comment) => {
   const videoId = window.location.href.split("/videos/")[1];
@@ -8,7 +10,20 @@ const sendComment = async (comment) => {
     method: "POST",
     data: { comment },
   });
-  console.log(response);
+  if (response.status === 200) {
+    addComment(comment);
+  }
+};
+const addComment = (comment) => {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerHTML = comment;
+  li.appendChild(span);
+  commentList.prepend(li);
+  increaseNumber();
+};
+const increaseNumber = () => {
+  commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1;
 };
 
 const handleSubmit = (e) => {
