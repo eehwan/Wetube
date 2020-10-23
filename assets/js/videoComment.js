1f8b75fa2e30634681278ad58b13ece61e9c5f6b
eehwan <eehwan.shin@gmail.com>
2020-10-24 03:56:25 +0900

    added deleteComment function - on the frontend

--- assets/js/videoComment.js
+++ assets/js/videoComment.js
@@ -11,20 +11,44 @@ const sendComment = async (comment) => {
     data: { comment },
   });
   if (response.status === 200) {
-    addComment(comment);
+    addComment(comment, response.data.userName, response.data.commentId);
   }
 };
-const addComment = (comment) => {
+const addComment = (comment, userName, commentId) => {
   const li = document.createElement("li");
   const UserNameField = document.createElement("a");
   const TextField = document.createElement("span");
+  const DeleteBtn = document.createElement("a");
+  const hiddenField = document.createElement("div");
   UserNameField.href = `/users/me`;
-  UserNameField.innerText = "me";
+  UserNameField.innerText = `${userName}`;
   TextField.innerHTML = comment;
+  DeleteBtn.innerText = "❌";
+  hiddenField.innerHTML = commentId;
+  hiddenField.style.display = "none";
+  DeleteBtn.addEventListener("click", deleteComment);
   li.appendChild(UserNameField);
   li.appendChild(TextField);
+  li.appendChild(DeleteBtn);
+  li.appendChild(hiddenField);
   commentList.prepend(li);
   increaseNumber();
+};
+const deleteComment = async (event) => {
+  const videoId = window.location.href.split("/videos/")[1];
+  const commentId = event.target.parentNode.lastChild.innerHTML;
+  const response = await axios({
+    url: `/api/${videoId}/${commentId}/delete`,
+    method: "POST",
+  });
+  if (response.status === 200) {
+    deleteLi(event.target);
+  }
+};
+const deleteLi = (target) => {
+  const li = target.parentNode;
+  const ul = li.parentNode;
+  ul.removeChild(li);
 };
 const increaseNumber = () => {
   commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1;
