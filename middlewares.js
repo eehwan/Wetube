3c5bcff7b0fd6b3b057f05f4114ce35a57685ab7
eehwan <eehwan.shin@gmail.com>
2020-10-23 18:23:28 +0900

    edited storage to aws-s3

--- middlewares.js
+++ middlewares.js
@@ -1,9 +1,29 @@
 import routes from "./routes";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 import multer from "multer";
 
-const uploadDir = "uploads/";
-const multerVideo = multer({ dest: `${uploadDir}videos/` });
-const multerAvatar = multer({ dest: `${uploadDir}avatars/` });
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_SECRET,
+});
+const multerVideo = multer({
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "wetube-eehwan/video",
+  }),
+});
+const multerAvatar = multer({
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "wetube-eehwan/avatar",
+  }),
+});
+// const uploadDir = "uploads/";
+// const multerVideo = multer({ dest: `${uploadDir}videos/` });
+// const multerAvatar = multer({ dest: `${uploadDir}avatars/` });
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
